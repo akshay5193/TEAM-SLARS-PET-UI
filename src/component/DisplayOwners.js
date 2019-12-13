2319a565bbf8100f5de5ec47c36c95a4dc26ea94
Akshay <akshaynande1993@gmail.com>
2019-12-13 13:23:31 -0600

    x

--- src/component/DisplayOwners.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class DisplayOwners extends Component {
-  render() {
-    return <div className="container"></div>;
-  }
-}
-
-export default DisplayOwners;
