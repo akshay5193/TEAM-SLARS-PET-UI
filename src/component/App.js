0b945b73a4515f0eaf046eb40cc6f83764b6e908
mdx919 <thanvirdx919@gmail.com>
2019-12-13 11:02:05 -0600

    init NavBar, ownerInfo, Home

---
+++ src/component/App.js
@@ -0,0 +1,33 @@
+//import logo from "./logo.svg";
+import React from 'react'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import DisplayOwners from './DisplayOwners';
+import Home from './Home';
+import Navbar from './Navbar';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+import OwnerInfo from './OwnerInfo';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/owners">
+            <OwnerInfo  />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
