b328d36a6a16f3d12eea38b0ec78702d5916d97b
Akshay <akshaynande1993@gmail.com>
2019-12-13 11:39:35 -0600

    x

--- src/component/App.js
+++ src/component/App.js
@@ -1,29 +1,24 @@
 //import logo from "./logo.svg";
-import React from 'react'
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import DisplayOwners from './DisplayOwners';
-import Home from './Home';
-import Navbar from './Navbar';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from 'react-router-dom'
-import OwnerInfo from './OwnerInfo';
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import DisplayOwners from "./DisplayOwners";
+import Home from "./Home";
+import Navbar from "./Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import OwnerInfo from "./OwnerInfo";
 
 function App() {
   return (
-    
     <Router>
       <div>
-        <Navbar/> 
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
           <Route path="/owners">
-            <DisplayOwners  />
+            <DisplayOwners />
           </Route>
         </Switch>
       </div>
